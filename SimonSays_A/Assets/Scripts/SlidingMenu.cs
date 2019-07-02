6f30bb4e1ffe02b323d85f1a1eceed4844d6e924
lickey10 <lickey10@gmail.com>
2019-07-02 12:40:59 -0600

    first import 5

---
+++ SimonSays_A/Assets/Scripts/SlidingMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (Input.GetMouseButtonDown(0) && touch.position.x < Screen.width / 2)
+            {
+                //slide menu out
+
+            }
+        }
+        
+    }
+}
