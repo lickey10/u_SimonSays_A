050958648fa82b65f9e6094b8632fae7f77ffff6
lickey10 <lickey10@gmail.com>
2019-07-03 20:10:44 -0600

    first import 11

---
+++ SimonSays_A/Assets/Models/Characters/Jack/Scripts/JackDemo.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JackDemo : MonoBehaviour {
+
+	public Animator animator;
+	public string AnimationCategory1 = "Common";//walk, run, idle, jump
+	public string AnimationCategory2 = "Fun";//cool things to show off
+	public string AnimationCategory3 = "Character Specific";//Custom movements for this character - fight, dance, climb, box, etc
+	
+	void OnGUI() {
+
+		GUILayout.BeginVertical("box");
+		if (GUILayout.Button(AnimationCategory1)) {
+			if(animator.GetBool("Common"))
+				animator.ResetTrigger("Common");
+			else
+			{
+				animator.ResetTrigger("Fun");
+				animator.ResetTrigger("Character Specific");
+				animator.SetTrigger("Common");
+			}
+		}
+		if (GUILayout.Button(AnimationCategory2)) {
+			if(animator.GetBool("Fun"))
+				animator.ResetTrigger("Fun");
+			else
+			{
+				animator.ResetTrigger("Character Specific");
+				animator.ResetTrigger("Common");
+				animator.SetTrigger("Fun");
+			}
+		}
+		if (GUILayout.Button(AnimationCategory3)) {
+			if(animator.GetBool("Character Specific"))
+				animator.ResetTrigger("Character Specific");
+			else
+			{
+				animator.ResetTrigger("Common");
+				animator.ResetTrigger("Fun");
+				animator.SetTrigger("Character Specific");
+
+				//the animation isn't moving past this one if the bool "Character Specific" is set.  
+				//It should because I have it set to move on after the first animation is done but it doesn't want to.  
+				//I wonder if it's because the first animation is so small
+				//this will set the bool "Character Specific" to false
+				Invoke("resetTrigger",2f);
+			}
+		}
+		GUILayout.FlexibleSpace();
+		GUILayout.FlexibleSpace();
+		GUILayout.EndVertical();
+	}
+
+	private void resetTrigger()
+	{
+		animator.ResetTrigger("Character Specific");
+	}
+}
---
+++ SimonSays_A/Assets/Models/Characters/Jack/readme.txt
@@ -0,0 +1,34 @@
+This is a fully rigged character.  Jack is Mechanim ready!  He has 107 bones and muscles. Use the "Jack Demo" scene to watch Jack perform some of the 2500+ free MoCap animations available on the asset store.
+
+Press play and sit and watch the animation show.  You can also choose which type of animation you would like to view using the 3 buttons in the upper left corner. 
+
+Requires Unity 3.5.7 or higher.
+
+Perfect for your adventure game development! This fully rigged and animated character will work great for your game! 
+
+Optimized for real-time engines and suitable for high quality renders. Works great for mobile apps.
+
+Verts:25840
+Faces:23087
+Tris:46174 
+
+The Rigged Model Contains 107 bones and is fully articulated with 17 animations: 
+
+-Idle
+-Walk
+-2 different kinds of ducking 
+-Run
+-2 kinds of attack 
+-Die
+-Get up from ground
+-Hit in back
+-Jump
+-Strafe left and right
+-5 fun animations (Chicken dance, a Gorilla, Moonwalking, Surprised and Stretch and Yawn)
+
+Now with face rig.
+
+Let me know if you have any comments or suggestions.
+
+Thanks,
+SnickleFritz
